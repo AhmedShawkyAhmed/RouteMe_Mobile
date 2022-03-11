c68ca648c2bdfd7ee118cd4d5428743d5ca5ee7b
Ahmed hassan <80314524+AhmedHassanHamid@users.noreply.github.com>
2022-03-11 23:57:24 +0200

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/screens/splash_screen.dart';
 import 'package:mobile/styles/colors.dart';
 import 'package:mobile/styles/colors.dart';
 import 'package:mobile/widgets/buttom_nav_bar.dart';
 import 'package:sizer/sizer.dart';
 import 'screens/Driver/end_task_screen.dart';
 import 'screens/Driver/task_details_screen.dart';
+import 'screens/Login.dart';
+import 'screens/Setpassword.dart';
 import 'screens/vendor/branch_screen.dart';
 import 'screens/vendor/google_maps_screen.dart';
+import 'screens/verify.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +28,7 @@ class MyApp extends StatelessWidget {
           title: 'Route Me',
           theme: ThemeData(
               fontFamily: 'cairo', scaffoldBackgroundColor: AppColors.white),
-          home: TaskSetailsScreen(),
+          home: setpassword(),
         );
       },
     );
--- lib/screens/verify.dart
+++ lib/screens/verify.dart
@@ -5,9 +5,12 @@ import 'package:mobile/styles/colors.dart';
 import 'package:mobile/widgets/default_app_button.dart';
 import 'package:mobile/widgets/default_password_field.dart';
 import 'package:mobile/widgets/default_text_field.dart';
+import 'package:otp_text_field/otp_field_style.dart';
 import 'package:sizer/sizer.dart';
 import 'package:otp_text_field/otp_field.dart';
 import 'package:otp_text_field/style.dart';
+
+
 class verify extends StatefulWidget {
   const verify({Key? key}) : super(key: key);
 
@@ -24,77 +27,89 @@ class _verifyState extends State<verify> {
 
       backgroundColor: AppColors.darkBlue,
 
-      body: Padding(
-        padding: EdgeInsets.only(top:30.h),
-        child: Container(
-          width: 100.w,
-          height: 70.h,
-
-          decoration: BoxDecoration(
-            color: AppColors.white,
-            borderRadius: BorderRadius.only(
-                topLeft:Radius.circular(50),
-                topRight: Radius.circular(50)
-
-            ),
-          ),
-
-          child:Container(
-            width: double.infinity,
-            child: Column(
-
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 30,),
-                Padding(
-                  padding: const EdgeInsets.only(bottom : 5.0,left: 45),
-                  child: Text('Please Enter the verification Code that was sent via',style: TextStyle(fontSize: 34),),
-
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            crossAxisAlignment:CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset('assets/Mask_Group_1.png',),
+              Padding(
+                padding: const EdgeInsets.only(top:40),
+                child: Image.asset('assets/Group_10.png',height: 160,),
+              ),
+              Image.asset('assets/Mask_Group_2.png',),
+          ],),
+            Container(
+              width: 100.w,
+              height: 82.h,
+      
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft:Radius.circular(50),
+                    topRight: Radius.circular(50)
+      
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 80.0,right: 170),
-                  child: Text('example@gmail.com', style: TextStyle(fontSize: 25),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 300.0,bottom: 10),
-                  child: Text('Verification Code' , style: TextStyle(fontSize: 22),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 0.0),
-                  child: OTPTextField(
-                    length: 6,
-                    width: MediaQuery.of(context).size.width,
-                    fieldWidth: 60,
-                    style: TextStyle(
-                        fontSize: 25
+              ),
+      
+              child:Container(
+                width: double.infinity,
+                child: Column(
+      
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                 
+                    Padding(
+                      padding: const EdgeInsets.only(left: 35,top: 35,),
+                      child: Text('Please Enter the verification Code that was sent via',style: TextStyle(fontSize: 25,fontWeight:FontWeight.bold),),
+      
                     ),
-                    textFieldAlignment: MainAxisAlignment.spaceAround,
-                    fieldStyle: FieldStyle.underline,
-                    onCompleted: (pin) {
-                      print("Completed: " + pin);
-                    },
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 90,bottom: 50,),
+                      child: Text('example@gmail.com', style: TextStyle(fontSize: 25),),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right:200,bottom: 7,),
+                      child: Text('Verification Code' , style: TextStyle(fontSize: 22),),
+                    ),
+                    OTPTextField(
+                      otpFieldStyle: OtpFieldStyle(backgroundColor:AppColors.lightGray),
+                      outlineBorderRadius: 15,
+                      length: 6,
+                      width: MediaQuery.of(context).size.width,
+                      fieldWidth: 43,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                          fontSize: 25
+                      ),
+                      textFieldAlignment: MainAxisAlignment.spaceAround,
+                      onCompleted: (pin) {
+                        print("Completed: " + pin);
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top:170),
+                      child: DefaultAppButton(
+                        text: 'Verify',
+                        backGround: AppColors.blue,
+                        fontSize: 30,
+                        height: 10.h,
+                        onTap: (){},
+                        width: 48.w,
+                        textColor: AppColors.white ,),
+                    ),
+      
+      
+      
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 150.0),
-                  child: DefaultAppButton(
-                    text: 'Verify',
-                    backGround: AppColors.blue,
-                    fontSize: 30,
-                    height: 10.h,
-                    onTap: (){},
-                    width: 48.w,
-                    textColor: AppColors.white ,),
-                ),
-
-
-
-              ],
-            ),
-          )  ,
-          ),
-
+              )  ,
+              ),
+          ],
+        ),
       ),
     );
   }
