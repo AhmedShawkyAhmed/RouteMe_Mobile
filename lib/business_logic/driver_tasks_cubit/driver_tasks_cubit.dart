20afd4c5dd55dd3dc7c06fe7bbc3f649294b9b74
Ahmed hassan <80314524+AhmedHassanHamid@users.noreply.github.com>
2022-04-14 22:55:34 +0200

    driver_tasks

---
+++ lib/business_logic/driver_tasks_cubit/driver_tasks_cubit.dart
@@ -0,0 +1,43 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:mobile/constants/end_points.dart';
+import 'package:mobile/data/local/cache_helper.dart';
+import 'package:mobile/data/models/get_driver_tasks_model.dart';
+import 'package:mobile/data/network/responses/get_driver_tasks_response.dart';
+import 'package:mobile/data/remote/dio_helper.dart';
+
+part 'driver_tasks_state.dart';
+
+class DriverTasksCubit extends Cubit<List<MyTasksModel>> {
+  
+  DriverTasksCubit() : super([]);
+
+  static DriverTasksCubit get(context) => BlocProvider.of(context);
+
+  MyTasksResponse? myTasksResponse;
+
+  Future getDriverTasks() async {
+    await DioHelper.postData(
+      url: tasks,
+      body: {
+        'driverId': CacheHelper.getDataFromSharedPreference(key: "userId"),
+      },
+    ).then((value) {
+      //print(value.data);
+      final myData = Map<String, dynamic>.from(value.data);
+      myTasksResponse = MyTasksResponse.fromJson(myData);
+      if (myTasksResponse!.status == 200) {
+        print(myTasksResponse!.tasks![0].clientName);
+        return myTasksResponse!.tasks;
+      } else {
+        print(myTasksResponse!.message);
+        return myTasksResponse!.message;
+      }
+    }).catchError((error) {
+      print(error.toString());
+    });
+    return myTasksResponse!.tasks;
+  }
+  void get myTasks async => emit(await getDriverTasks());
+}
---
+++ lib/data/models/get_driver_tasks_model.dart
@@ -0,0 +1,85 @@
+class MyTasksModel {
+
+  int id;
+  String orderNumber;
+  int dispatcherId;
+  int driverId;
+  String clientName;
+  String clientPhone;
+  int itemCount;
+  double price;
+  int vendorId;
+  int branchId;
+  double lon;
+  double lat;
+  String address;
+  String start;
+  String end;
+  String comment;
+  String status;
+
+  MyTasksModel({
+    required this.id,
+    required this.orderNumber,
+    required this.dispatcherId,
+    required this.driverId,
+    required this.clientName,
+    required this.clientPhone,
+    required this.itemCount,
+    required this.price,
+    required this.vendorId,
+    required this.branchId,
+    required this.lon,
+    required this.lat,
+    required this.address,
+    required this.start,
+    required this.end,
+    required this.comment,
+    required this.status,
+  });
+
+
+  factory MyTasksModel.fromJson(Map<String, dynamic> json) => MyTasksModel(
+    id: json["id"],
+    orderNumber: json["orderNumber"],
+    dispatcherId: json["dispatcherId"],
+    driverId: json["driverId"],
+    lon: json['lon'] == null ? 0.0 : json['lon'].toDouble(),
+    lat: json['lat'] == null ? 0.0 : json['lat'].toDouble(),
+    clientName: json["clientName"],
+    price: json['price'] == null ? 0.0 : json['price'].toDouble(),
+    clientPhone: json["clientPhone"],
+    itemCount: json["itemCount"],
+    comment: json["comment"],
+    vendorId: json["vendorId"],
+    branchId: json["branchId"],
+    address: json["address"],
+    start: json["start"],
+    end: json["end"],
+    status: json["status"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "orderNumber": orderNumber,
+    "dispatcherId": dispatcherId,
+    "driverId": driverId,
+    "comment": comment,
+    "itemCount": itemCount,
+    "clientName": clientName,
+    "clientPhone": clientPhone,
+    "lon": lon,
+    "lat": lat,
+    "address": address,
+    "price": price,
+    "vendorId": vendorId,
+    "branchId": branchId,
+    "start":start,
+    "end":end,
+    "comment": comment,
+    "status": status,
+
+  };
+
+
+}
